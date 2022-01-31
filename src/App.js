e9aa3476837eb2e20e2b9f71d5359d17e2956017
tharikuthuman <thaarik@gmail.com>
2022-02-01 01:13:54 +0530

    task-subbmission-client

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, Card, Form, formData } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Fileupload/>
     </div>
   );
 }
 
 export default App;
+
+
+function Fileupload(){
+
+  const [name, setName] = useState("");
+  const [singleFile, setSingleFile] = useState('');
+
+  const uploadFile = async() => {
+
+    //console.log(singleFile);
+  }
+    // getting api endpoint
+    const apiUrl = 'http://localhost:9000/api/';
+
+    const singleFileUpload = async (data) => {
+      try{
+        await axios.post( apiUrl + 'singleFile', data)
+      } catch (error){
+        throw error;
+      }
+    }
+
+
+
+  // const upload = (event) => {
+  //   const data = new FormData();
+
+  //   data.append('name', name);
+  //   data.append('file', file);
+
+  //   fetch.post('https://httpbin.org/anything', data)
+  //   .then(res => console.log(res))
+  //   .catch(err => console.log(err));
+
+  // };
+  
+  return(
+
+    <div className='card'>
+      <Card bg='info'>
+          <Card.Body>
+            <Form>
+               <Form.Group controlId="formFileLg" className="mb-3">
+                  <Form.Label className='formLabel'>Student Task Submission Portal</Form.Label>
+                  <Form.Control  type="text" onChange={(e) => setName(e.target.value)} placeholder="Enter name" />
+                  <Form.Control type="file" onChange={(e) => setSingleFile(e.target.files[0])} size="lg" />
+                </Form.Group>
+            </Form>
+
+          </Card.Body>
+      </Card>
+
+        <Button className='btn'  onClick={() => uploadFile()}  variant="primary" size="lg" active>
+          Click here to Upload
+        </Button>{' '}
+
+    </div>   
+    
+
+  )
+}
